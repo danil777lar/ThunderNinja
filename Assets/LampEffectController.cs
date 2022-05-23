bda37830e14727e439452f4ffefc1e67f1d63865
daniil.larzhevskiy <daniil.larzhevskiy@playcus.com>
2022-05-23 18:53:24 +0300

    lamp godray effect

---
+++ Assets/LampEffectController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampEffectController : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _parts;
+    [SerializeField] private Light _light;
+    [SerializeField] private Transform _raycastRoot;
+
+    private void Start()
+    {
+        
+    }
+}
